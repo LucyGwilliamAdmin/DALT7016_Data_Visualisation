25349bec58e734f1e8775c8ed67a1af868069ae5
LucyGwilliamAdmin <lucy.gwilliam@ons.gov.uk>
2021-07-12 14:28:38 +0100

    more changes

--- data_prep.R
+++ data_prep.R
@@ -15,33 +15,96 @@ for (package in required_packages){
 # Load libraries
 lapply(required_packages, library, character.only = TRUE)
 
-if (file.exists("Data/final_data.csv")==FALSE){
-  source("collate_data.R")
-  # reset working
-  workingdir<-"C:/Users/3567890/Documents/Uni/Year 2/Semester 2/Data Visualisation/DALT7016_Data_Visualisation"
-  setwd(workingdir)
-}
+# if (file.exists("Data/final_data.csv")==FALSE){
+#   source("collate_data.R")
+#   # reset working
+#   workingdir<-"C:/Users/3567890/Documents/Uni/Year 2/Semester 2/Data Visualisation/DALT7016_Data_Visualisation"
+#   setwd(workingdir)
+# }
 
+all_data<-read.csv("Data/final_data.csv", fileEncoding = "UTF-8-BOM") 
 
+all_data$Sex <- factor(all_data$Sex,levels = c("All", "Female", "Male"))
+all_data$WorkingPattern <- factor(all_data$WorkingPattern, levels = c("All", "Full-time", "Part-time"))
+all_data$AgeGroup <- factor(all_data$AgeGroup, levels = c("All", "16-17", "16-19", "18-21", "20-24", "22-29", "25-29", "30-34",
+                                                          "30-39", "35-39", "40-44", "40-49", "45-49", "50-54", "50-59",
+                                                          "55-59", "60-64", "60+"))
+all_data$Occupation <- factor(all_data$Occupation, levels = c("All", "Administrative and secretarial occupations",
+                                                              "Associate professional and technical occupations",
+                                                              "Caring, leisure and other service occupations",
+                                                              "Elementary occupations",
+                                                              "Managers, directors and senior officials",
+                                                              "Process, plant and machine operatives",
+                                                              "Professional occupations",
+                                                              "Sales and customer service occupations",
+                                                              "Skilled trades occupations"))
+all_data$Ethnicity <- factor(all_data$Ethnicity, levels = c("All", "White", "Mixed", "Mixed/multiple ethnic groups", "Asian", "Indian",
+                                                            "Pakistani", "Bangladeshi", "Chinese", "Any other Asian background",
+                                                            "Black", "Black/African/Caribbean/Black British",
+                                                            "Other ethnic group", "Other"))
+all_data$WorkRegion <- factor(all_data$WorkRegion, levels = c("All", "Wales", "Scotland", "Northern Ireland", "North East",
+                                                              "North West", "Yorkshire and The Humber", "East Midlands",
+                                                              "West Midlands", "East", "London", "South East", "South West"))
+all_data$Industry <- factor(all_data$Industry, levels = c("All", LETTERS[1:21]))
 
-all_data<-read.csv("Data/final_data.csv", fileEncoding = "UTF-8-BOM")
+ind_map<-list("A"="(Agriculture, Forestry and Fishing)",
+              "B"="(Mining and Quarrying)",
+              "C"="(Manufacturing)",
+              "D"="(Electricity, Gas, Steam and Air Conditioning Supply)",
+              "E"="(Water Supply; Sewerage, Waste Management and Remediation Activities)",
+              "F"="(Construction)",
+              "G"="(Wholesale and Retail Trade; Repair of Motor Vehicles and Motorcycles)",
+              "H"="(Transportation and Storage)",
+              "I"="(Accommodation and Food Service Activities)",
+              "J"="(Information and Communication)",
+              "K"="(Financial and Insurance Activities)",
+              "L"="(Real Estate Activities)",
+              "M"="(Professional, Scientific and Technical Activities)",
+              "N"="(Administrative and Support Service Activities)",
+              "O"="(Public Administration and Defence; Compulsory Social Security)",
+              "P"="(Education)",
+              "Q"="(Human Health and Social Work Activities)",
+              "R"="(Arts, Entertainment and Recreation)",
+              "S"="(Other Service Activities)",
+              "T"="(Activities of Households as Employers; Undifferentiate Goods and Services Producing Activities of Households for Own Use)",
+              "U"="(Activities of Extraterritorial Organisations and Bodies)")
+
+ind_map2<-list("All"="",
+               "A"="(Agriculture, Forestry and Fishing)",
+               "B"="(Mining and Quarrying)",
+               "C"="(Manufacturing)",
+               "D"="(Electricity, Gas, Steam and Air Conditioning Supply)",
+               "E"="(Water Supply; Sewerage, Waste Management and Remediation Activities)",
+               "F"="(Construction)",
+               "G"="(Wholesale and Retail Trade; Repair of Motor Vehicles and Motorcycles)",
+               "H"="(Transportation and Storage)",
+               "I"="(Accommodation and Food Service Activities)",
+               "J"="(Information and Communication)",
+               "K"="(Financial and Insurance Activities)",
+               "L"="(Real Estate Activities)",
+               "M"="(Professional, Scientific and Technical Activities)",
+               "N"="(Administrative and Support Service Activities)",
+               "O"="(Public Administration and Defence; Compulsory Social Security)",
+               "P"="(Education)",
+               "Q"="(Human Health and Social Work Activities)",
+               "R"="(Arts, Entertainment and Recreation)",
+               "S"="(Other Service Activities)",
+               "T"="(Activities of Households as Employers; Undifferentiate Goods and Services Producing Activities of Households for Own Use)",
+               "U"="(Activities of Extraterritorial Organisations and Bodies)")
 
 paygap_data<-all_data %>%
   filter(Units %in% c("Disability pay gap (%)", "Ethnicity pay gap (%)", "Gender pay gap (%)"))
 
+paygap_data<-droplevels(paygap_data)
+
 comparison_data<-all_data %>%
   filter(Units=="Median")
 
-#test<-read.csv("Data/paygapdata.csv", fileEncoding = "UTF-8-BOM")
-#test2<-read.csv("Data/compare_dataset.csv", fileEncoding = "UTF-8-BOM")
+comparison_data<-droplevels(comparison_data)
 
-#paygap_data[paygap_data==""]<-"Total"
-#comparison_data[comparison_data==""]<-"Total"
-
-#comparison_data$Value<-as.numeric(comparison_data$Value)
 
 available_breakdowns_gender<-list("Working pattern"="WorkingPattern", "Age group"="AgeGroup",
-                               "Work region"="WorkRegion", "Occupation"="Occupation")
+                                  "Work region"="WorkRegion", "Occupation"="Occupation")
 
 available_breakdowns_ethnicity<-list("Ethnicity"="Ethnicity")
 
@@ -55,64 +118,61 @@ available_breakdowns<-list("Gender pay gap (%)"=available_breakdowns_gender,
                            "Disability pay gap (%)"=available_breakdowns_disability)
 
 total_data<-comparison_data %>%
-  filter_at(vars(names(comparison_data)[!names(comparison_data) %in% c("Year", "Value", "Units", "comment")]), all_vars(.=="Total"))
+  filter_at(vars(names(comparison_data)[!names(comparison_data) %in% c("Year", "Value", "Units", "comment")]), all_vars(.=="All"))
 
 
-shape_set<-c(16,16,16,16,16,16,
-             17,17,17,17,17,17,
-             15,15,15,15,15,15,
-             18,18,18,18,18,18)
-
-create_shape_set<-function(dataset, colname){
-  shapes<-head(shape_set, length(levels(as.factor(dataset[[colname]])))-1)
-  names(shapes)<-levels(as.factor(dataset[[colname]]))[levels(as.factor(dataset[[colname]]))!="Total"]
-  shapes[["Total"]]<-16
-  shapeScale<-scale_shape_manual(name = colname ,values = shapes, labels = function(x) str_wrap(x, width = 27))
-  return(shapeScale)
-}
+shape_set<-c(rep("circle", times=7),
+             rep("star-triangle-up", times=6),
+             rep("square", times=6),
+             rep("diamond", times=6))
 
 
-create_size_set<-function(dataset, colname){
-  sizes<-head(c(rep(3, times = 18), rep(4, times=6)), length(levels(as.factor(dataset[[colname]])))-1)
-  names(sizes)<-levels(as.factor(dataset[[colname]]))[levels(as.factor(dataset[[colname]]))!="Total"]
-  sizes[["Total"]]<-3
-  sizeScale<-scale_size_manual(name = colname ,values = sizes, labels = function(x) str_wrap(x, width = 27))
-  return(sizeScale)
-}
-
-
-colour_set<-c('#cd7a00', '#339966', '#9966cc', '#8d4d57', '#A33600', '#054ce6',
+colour_set<-c("#00006A", '#cd7a00', '#339966', '#9966cc', '#8d4d57', '#A33600', '#054ce6',
               '#cd7a00', '#339966', '#9966cc', '#8d4d57', '#A33600', '#054ce6',
               '#cd7a00', '#339966', '#9966cc', '#8d4d57', '#A33600', '#054ce6',
               '#cd7a00', '#339966', '#9966cc', '#8d4d57', '#A33600', '#054ce6')
 
-create_colour_set<-function(dataset, colname){
-  colours<-head(colour_set, length(levels(as.factor(dataset[[colname]])))-1)
-  names(colours)<-levels(as.factor(dataset[[colname]]))[levels(as.factor(dataset[[colname]]))!="Total"]
-  colours[["Total"]]<-"#00006A"
-  colScale<-scale_colour_manual(name = colname ,values = colours, labels = function(x) str_wrap(x, width = 27))
-  return(colScale)
+gap_colour_set<-c('#00006Abf', '#cd7a00bf', '#339966bf', '#9966ccbf', '#8d4d57bf', '#A33600bf', '#054ce6bf',
+                  '#cd7a00bf', '#339966bf', '#9966ccbf', '#8d4d57bf', '#A33600bf', '#054ce6bf',
+                  '#cd7a00bf', '#339966bf', '#9966ccbf', '#8d4d57bf', '#A33600bf', '#054ce6bf',
+                  '#cd7a00bf', '#339966bf', '#9966ccbf', '#8d4d57bf', '#A33600bf', '#054ce6bf')
+
+size_set<-c(rep(3, times = 7), rep(4, times=6), rep(3, times=12))
+
+sexColours <- setNames(head(colour_set, length(levels(as.factor(comparison_data[["Sex"]])))), levels(as.factor(comparison_data[["Sex"]])))
+wpColours <- setNames(head(colour_set, length(levels(as.factor(comparison_data[["WorkingPattern"]])))), levels(as.factor(comparison_data[["WorkingPattern"]])))
+ageColours <- setNames(head(colour_set, length(levels(as.factor(comparison_data[["AgeGroup"]])))), levels(as.factor(comparison_data[["AgeGroup"]])))
+wrColours <- setNames(head(colour_set, length(levels(as.factor(comparison_data[["WorkRegion"]])))), levels(as.factor(comparison_data[["WorkRegion"]])))
+occColours <- setNames(head(colour_set, length(levels(as.factor(comparison_data[["Occupation"]])))), levels(as.factor(comparison_data[["Occupation"]])))
+indColours <- setNames(head(colour_set, length(levels(as.factor(comparison_data[["Industry"]])))), levels(as.factor(comparison_data[["Industry"]])))
+
+sexShapes <- setNames(head(shape_set, length(levels(as.factor(comparison_data[["Sex"]])))), levels(as.factor(comparison_data[["Sex"]])))
+wpShapes <- setNames(head(shape_set, length(levels(as.factor(comparison_data[["WorkingPattern"]])))), levels(as.factor(comparison_data[["WorkingPattern"]])))
+ageShapes <- setNames(head(shape_set, length(levels(as.factor(comparison_data[["AgeGroup"]])))), levels(as.factor(comparison_data[["AgeGroup"]])))
+wrShapes <- setNames(head(shape_set, length(levels(as.factor(comparison_data[["WorkRegion"]])))), levels(as.factor(comparison_data[["WorkRegion"]])))
+occShapes <- setNames(head(shape_set, length(levels(as.factor(comparison_data[["Occupation"]])))), levels(as.factor(comparison_data[["Occupation"]])))
+indShapes <- setNames(head(shape_set, length(levels(as.factor(comparison_data[["Industry"]])))), levels(as.factor(comparison_data[["Industry"]])))
+
+graph_titles<-c("Gender pay gap (%)"="Gender pay gap median (comparison group = men)", "Ethnicity pay gap (%)"="Ethnicity pay gap median (comparison group = white employees)", "Disability pay gap (%)"="Disability pay gap median (comparison group = employees without a disability)")
+
+submenu_mapping<-c("sex"="Sex", "wp"="WorkingPattern", "age"="AgeGroup", "wr"="WorkRegion", "occ"="Occupation", "ind"="Industry")
+
+data_download<-function(variable){
+  data<-comparison_data %>%
+    filter(eval(parse(text=variable)) %in% unique(comparison_data[[variable]])) %>%
+    filter_at(vars(names(comparison_data)[!names(comparison_data) %in% c("Year", "Units", "Value", variable, "comment")]), all_vars(.=="All")) %>%
+    select(Year,where(~length(unique(.))>1),comment)
+  return(data)
 }
 
 
-sexColours <- create_colour_set(comparison_data, "Sex")
-ageColours <- create_colour_set(comparison_data, "AgeGroup")
-wpColours <- create_colour_set(comparison_data, "WorkingPattern")
-occColours <- create_colour_set(comparison_data, "Occupation")
-wrColours <- create_colour_set(comparison_data, "WorkRegion")
-indColours <- create_colour_set(comparison_data, "Industry")
 
-sexShapes <- create_shape_set(comparison_data, "Sex")
-ageShapes <- create_shape_set(comparison_data, "AgeGroup")
-wpShapes <- create_shape_set(comparison_data, "WorkingPattern")
-occShapes <- create_shape_set(comparison_data, "Occupation")
-wrShapes <- create_shape_set(comparison_data, "WorkRegion")
-indShapes <- create_shape_set(comparison_data, "Industry")
+sex_data_download<-data_download("Sex")
+wp_data_download<-data_download("WorkingPattern")
+age_data_download<-data_download("AgeGroup")
+wr_data_download<-data_download("WorkRegion")
+occ_data_download<-data_download("Occupation")
+ind_data_download<-data_download("Industry")
 
-sexSizes <- create_size_set(comparison_data, "Sex")
-ageSizes <- create_size_set(comparison_data, "AgeGroup")
-wpSizes <- create_size_set(comparison_data, "WorkingPattern")
-occSizes <- create_size_set(comparison_data, "Occupation")
-wrSizes <- create_size_set(comparison_data, "WorkRegion")
-indSizes <- create_size_set(comparison_data, "Industry")
 
+download_text <- "Download chart data as CSV"
